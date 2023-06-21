78c24db8cf6a09b2911d4b43f77271950e85a0b0
dwikipeddos <arisxxx5@gmail.com>
2023-06-21 14:10:39 +0800

    naming convention

--- src/PeddosLaravelToolsServiceProvider.php
+++ src/PeddosLaravelToolsServiceProvider.php
@@ -8,7 +8,7 @@ class PeddosLaravelToolsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->publishes([__DIR__ . '/../config/PeddosPermissionConfig.php' => config_path('PeddosPermissionConfig.php')], 'peddos-laravel-tool-config');
+        $this->publishes([__DIR__ . '/../config/laraveltoolspermission.php' => config_path('laraveltoolspermission.php')], 'peddos-laravel-tools-config');
         $this->publishes([
             __DIR__ . '/../database/migrations/' => database_path('migrations')
         ], 'peddos-laravel-tools-migration');
