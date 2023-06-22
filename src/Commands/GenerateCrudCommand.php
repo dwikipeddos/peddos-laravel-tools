d2f75db3f5e1fb48c19e91631855878a975cac6e
dwikipeddos <arisxxx5@gmail.com>
2023-06-22 10:52:54 +0800

    added stubs and generating tools

---
+++ src/Commands/GenerateCrudCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class GenerateCrudCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:all {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+
+    public function handle()
+    {
+        try {
+            $name = $this->argument('name');
+            $this->generateModel($name);
+            $this->generatePolicy($name);
+            $this->generateRequests($name);
+            $this->generateController($name);
+        } catch (Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    function generateModel(string $name): void
+    {
+        Artisan::call('make:model', ['name' => ucfirst($name), '-m' => true, '-f' => true]);
+    }
+
+    function generateController(string $name): void
+    {
+        $this->generateFileFromStub("Controller", "Controller", "Http/Controllers/", $name);
+    }
+
+    function generateRequests(string $name): void
+    {
+        $this->generateFileFromStub("StoreRequest", "StoreRequest", "Http/Requests/", $name);
+        $this->generateFileFromStub("UpdateRequest", "UpdateRequest", "Http/Requests/", $name);
+    }
+
+    function generatePolicy(string $name): void
+    {
+        $this->generateFileFromStub("Policy", "Policy", "Policies/", $name);
+    }
+
+
+    function generateFileFromStub(string $stub, string $filenameSuffix, string $fileLocation, string $name): void
+    {
+        $className = ucfirst($name) . $filenameSuffix;
+        $content = File::get(base_path("stubs/$stub.stub"));
+
+        $content = str_replace('{name}', Str::lower($name), $content);
+        $content = str_replace('{Name}', ucfirst($name), $content);
+
+        File::put(app_path("$fileLocation" . $className . '.php'), $content);
+        $this->info($className . "generated $className successfully!");
+    }
+}
--- src/PeddosLaravelToolsServiceProvider.php
+++ src/PeddosLaravelToolsServiceProvider.php
@@ -8,10 +8,16 @@ class PeddosLaravelToolsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        //config
         $this->publishes([__DIR__ . '/../config/peddoslaraveltools.php' => config_path('peddoslaraveltools.php')], 'peddos-laravel-tools-config');
+
+        //migrations
         $this->publishes([
             __DIR__ . '/../database/migrations/' => database_path('migrations')
         ], 'peddos-laravel-tools-migration');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        //stubs
+        $this->publishes([__DIR__ . '/../stubs/' => base_path('stubs'), 'peddos-laravel-tools-stubs']);
     }
 }
