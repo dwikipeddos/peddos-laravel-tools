912c43dfce0838bced6b3170fa04df5c7a70128a
dwikipeddos <arisxxx5@gmail.com>
2023-09-04 15:43:12 +0800

    fix role not inserted

--- src/Commands/UpdatePermissionRoleCommand.php
+++ src/Commands/UpdatePermissionRoleCommand.php
@@ -3,6 +3,7 @@
 namespace Dwikipeddos\PeddosLaravelTools\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -23,6 +24,20 @@ class UpdatePermissionRoleCommand extends Command
     protected $description = 'sync role permissions based on the config file';
 
     /**
+     * All Roles in database
+     * 
+     * @var Collection
+     */
+    protected $allRoles;
+
+    /**
+     * All permission in database
+     * 
+     * @var Collection
+     */
+    protected $allPermissions;
+
+    /**
      * Execute the console command.
      *
      * @return int
@@ -31,18 +46,23 @@ class UpdatePermissionRoleCommand extends Command
     {
         $permissions = config('peddoslaraveltools.available_permissions');
 
-        $allPermissions = Permission::all();
-        $allRoles = Role::all();
+        $this->allPermissions = Permission::all();
+        $this->allRoles = Role::all();
+
+        $this->insertMissingRoles($permissions);
+
+        //refresh roles
+        $this->allRoles = Role::all();
 
         foreach ($permissions as $permission) {
-            $permissionModel = $allPermissions->where('name', $permission['name'])->first();
+            $permissionModel = $this->allPermissions->where('name', $permission['name'])->first();
             if (!$permissionModel) {
                 $this->line('permission not found, creating permission ' . $permission['name'] . '...');
                 $permissionModel = Permission::create(['name' => $permission['name']]);
                 $this->line('permission created!');
             }
             $roles = $permission['roles'];
-            $roleModels = $allRoles->whereIn('name', $roles)->pluck('name')->toArray();
+            $roleModels = $this->allRoles->whereIn('name', $roles)->pluck('name')->toArray();
             if ($roleModels == null) {
                 $this->warn('role is not found when searching for permission ' . $permission['name']);
             }
@@ -52,5 +72,24 @@ class UpdatePermissionRoleCommand extends Command
 
         $this->info('Roles and permissions synced successfully!');
         return Command::SUCCESS;
+    }
+
+    function insertMissingRoles(array $configRoles): void
+    {
+        $missingRoles = collect($configRoles)->pluck('roles')->flatten()->unique()->diff($this->allRoles->pluck('name'));
+        if ($missingRoles->count() > 0) {
+            $newRoles = [];
+            foreach ($missingRoles as $missingRole) {
+                $this->line('found new role ' . $missingRole);
+                $newRoles[] = [
+                    'name' => $missingRole,
+                    'guard_name' => 'api',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+            Role::insert($newRoles);
+            $this->info("added new " . count($newRoles) . " roles");
+        }
     }
 }
